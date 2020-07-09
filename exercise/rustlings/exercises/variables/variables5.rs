153bcdf34469bedda1173cac39aff2ce0ba8dd80
tjj <1804232963@qq.com>
2020-07-09 22:31:07 +0800

    rustlings重传

---
+++ exercise/rustlings/exercises/variables/variables5.rs
@@ -0,0 +1,11 @@
+// variables5.rs
+// Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
+
+// I AM NOT DONE
+
+fn main() {
+    let mut number = 3;
+    println!("Number {}", number);
+    number = 4;
+    println!("Number {}", number);
+}
